5d904b948b17db8049ef77e7ad9581769971567c
Ken Kundert <thenurd@nurdletech.com>
2016-05-14 23:45:04 -0700

    Remove python2.6 from the list of supported versions.

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
