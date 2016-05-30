13277baf881e43e857bcab849d557af636e2240c
Ken Kundert <thenurd@nurdletech.com>
2016-05-30 13:08:33 -0700

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='engfmt',
-    version='0.6.1',
+    version='0.6.2',
     description='read and write in engineering notation',
     long_description=readme,
     author="Ken Kundert",
